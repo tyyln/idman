ec21b10b214d1c07859fc8b9808a4a9b10750840
tyyln <omertaylanmestanli@gmail.com>
2026-03-20 18:50:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-const CACHE_NAME = 'antrenman-v7';
-const ASSETS = [
+const CACHE_NAME = 'antrenman-v2';
+const REQUIRED_ASSETS = [
   './',
   './index.html',
   './manifest.json',
+];
+const OPTIONAL_ASSETS = [
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll(REQUIRED_ASSETS).then(() =>
+        Promise.allSettled(OPTIONAL_ASSETS.map(a => cache.add(a)))
+      )
+    )
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
+self.addEventListener('activate', event => {
+  event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
@@ -23,8 +29,14 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
